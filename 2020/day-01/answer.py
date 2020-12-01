9abfc1da3fe32a110c37d51ef2a396fbe04fbb35
Yannick PEROUX <yannick.peroux@gmail.com>
2020-12-01 12:11:10 +0100

    Add configuration for Black & run it

--- 2020/day-01/answer.py
+++ 2020/day-01/answer.py
@@ -6,7 +6,7 @@ print("-" * 80)
 print("Part 1")
 
 for i, entry in enumerate(report):
-    for other in report[i + 1:]:
+    for other in report[i + 1 :]:
         if entry + other == 2020:
             print(f"{entry} + {other} == 2020")
             print(f"Answer: {entry * other}")
@@ -15,8 +15,8 @@ print("-" * 80)
 print("Part 2")
 
 for i, entry in enumerate(report):
-    for j, second in enumerate(report[i + 1:]):
-        for third in report[i + j + 2:]:
+    for j, second in enumerate(report[i + 1 :]):
+        for third in report[i + j + 2 :]:
             if entry + second + third == 2020:
                 print(f"{entry} + {second} + {third} == 2020")
                 print(f"Answer: {entry * second * third}")
